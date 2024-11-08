c91c8f5c4a4a2b1c8c1ae0bba567d77b2a82676f
vishal007p <107975264+vishal007p@users.noreply.github.com>
2024-11-08 12:11:27 +0530

    changess

--- app/Http/Controllers/API/User/MaintanceBillController.php
+++ app/Http/Controllers/API/User/MaintanceBillController.php
@@ -10,8 +10,14 @@ class MaintanceBillController extends Controller
 {
     public function maintancebilllist(Request $request)
     {
-        $data =Maintancebill::with('maintancebilllists')->get();
-        return response([
+        $data = Maintancebill::with('maintancebilllists')
+        ->get()
+        ->map(function ($bill) {
+          
+            $bill->payment_status = $bill->maintancebilllists->isNotEmpty() ? 1 : 0;
+            return $bill;
+        });
+            return response([
             'message' => 'Maintance bill list displayed Successfully..!',  
             'data' => $data,  
             'statusCode' => 200
