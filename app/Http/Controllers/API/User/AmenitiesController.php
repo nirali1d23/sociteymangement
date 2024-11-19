50f83aeff85fe354c9b8c4761e43a6305a46d14b
vishal007p <107975264+vishal007p@users.noreply.github.com>
2024-11-19 14:47:42 +0530

    c

--- app/Http/Controllers/API/User/AmenitiesController.php
+++ app/Http/Controllers/API/User/AmenitiesController.php
@@ -30,7 +30,7 @@ class AmenitiesController extends Controller
                 $title = "🛎️ New Amenity Booking Request!";
                 $body = "📋 A new request for booking amenities has been submitted. Please review the details and take action. ✅ Approve or ❌ Disapprove the request now.";
                 
-                 $this->sendFirebaseNotification($fcmToken, $title, $body,null);
+                return   $this->sendFirebaseNotification($fcmToken, $title, $body,null);
             }
         
   
