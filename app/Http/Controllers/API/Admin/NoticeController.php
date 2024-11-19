e73740cb529056a99aa2f52a6e1a883006cb5242
vishal007p <107975264+vishal007p@users.noreply.github.com>
2024-11-19 19:17:35 +0530

    c

--- app/Http/Controllers/API/Admin/NoticeController.php
+++ app/Http/Controllers/API/Admin/NoticeController.php
@@ -74,6 +74,7 @@ class NoticeController extends Controller
             return $item;
         });
 
+        dd($data);
         return response([
             'message' => 'Notice Displayed Successfully..!',
             'data' => $data,
