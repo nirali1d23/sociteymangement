7560f3c198c55f77e35ebdb9b5d6e6144e919bfe
vishal007p <107975264+vishal007p@users.noreply.github.com>
2024-11-21 11:05:11 +0530

    c

--- app/Http/Controllers/API/Admin/MaintancerequestController.php
+++ app/Http/Controllers/API/Admin/MaintancerequestController.php
@@ -106,6 +106,7 @@ class MaintancerequestController extends Controller
     public function maintancestatus(Request $request)
     {
         $data = MaintanceProcess::with('staff')->where('maintance_request_id',$request->id)->get();
+        dd($data);
         if ($data) 
         {
             return response([
