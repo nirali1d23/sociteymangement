4d75c62346b3ba5ef46b32e471dad8ff6b298ff0
vishal007p <107975264+vishal007p@users.noreply.github.com>
2024-12-03 18:30:49 +0530

    c

--- app/Http/Controllers/API/Admin/AmenitiesController.php
+++ app/Http/Controllers/API/Admin/AmenitiesController.php
@@ -228,13 +228,13 @@ class AmenitiesController extends Controller
 
     //     return $item;
     // });
-
-    $data = Amenities::with('bookamenities')->get()->map(function ($item) {
+    $date =  date("Y-m-d");
+    $data = Amenities::with('bookamenities')->get()->map(function ($item) use($date) {
         if ($item->extra_time_status == 1) {
             // Generate morning and evening time slots
             $morningSlots = $this->generateTimeSlots($item->morning_start_time, $item->morning_end_time);
             $eveningSlots = $this->generateTimeSlots($item->evening_start_time, $item->evening_end_time);
-            $date =  date("Y-m-d");
+       
 
             // Get booked times from the database
             $bookedTimes = $item->bookamenities->where('date',$date)->map(function ($booking) {
