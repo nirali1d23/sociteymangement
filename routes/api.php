9906ba04537d08db0d6747904c4ae9a86ad5b4bc
= <=>
2024-12-23 11:45:34 +0530

    change

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::get('/user', function (Request $request)
 
 Route::controller(AuthController::class)->group(function() 
 {   
-    Route::post('loginnnnnnnnn', 'login');
+    Route::post('login', 'login');
     Route::post('register_rtw', 'register_rtw');
     Route::post('import', 'import');
     Route::post('changepassword', 'changepassword');
