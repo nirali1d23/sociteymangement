67e1698ea366d9e20d3f73a73a83b719896d24c7
= <=>
2026-02-18 16:36:28 +0530

    test

--- resources/views/admin_panel/layouts/sidebar.blade.php
+++ resources/views/admin_panel/layouts/sidebar.blade.php
@@ -17,12 +17,12 @@
         </a>
       </li>
 
-      <li class="nav-item"> 
+      {{-- <li class="nav-item"> 
         <a class="nav-link  @if(Request::segment(1) != 'flate') collapsed @endif " href="{{route('flate')}}">
           <i class="bi bi-shop"></i>
           <span>Flat</span>
         </a>
-      </li>
+      </li> --}}
 
       <li class="nav-item">
    
