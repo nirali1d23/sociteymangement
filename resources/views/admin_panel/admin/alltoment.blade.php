05f87564e643c6fae94af495aab94f264eb10367
= <=>
2026-02-18 15:04:59 +0530

    cc

--- resources/views/admin_panel/admin/alltoment.blade.php
+++ resources/views/admin_panel/admin/alltoment.blade.php
@@ -21,7 +21,6 @@
                 <tr>
                     <th>Username</th>
                     <th>Flat No</th>
-                    <th width="280px">Action</th>
                 </tr>
             </thead>
             <tbody></tbody>
@@ -34,17 +33,16 @@
             <div class="modal-content">
 
                 <div class="modal-header">
-                    <h4 class="modal-title">Create / Edit Allotment</h4>
+                    <h4 class="modal-title">Create Allotment</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
 
                 <div class="modal-body">
                     <form id="productForm">
-                        <input type="hidden" name="product_id" id="product_id">
 
                         <div class="mb-3">
                             <label>User</label>
-                            <select class="form-select" name="user_id">
+                            <select class="form-select" name="user_id" required>
                                 <option value="">Select User</option>
                                 @foreach($users as $user)
                                     <option value="{{ $user->id }}">{{ $user->name }}</option>
@@ -54,7 +52,7 @@
 
                         <div class="mb-3">
                             <label>Flat</label>
-                            <select class="form-select" name="flat_id">
+                            <select class="form-select" name="flat_id" required>
                                 <option value="">Select Flat</option>
                                 @foreach($flats as $flat)
                                     <option value="{{ $flat->id }}">{{ $flat->flat_number }}</option>
@@ -89,59 +87,30 @@ $(function () {
         }
     });
 
-    // DATATABLE
+    // DATATABLE (NO ACTION COLUMN)
     var table = $('.data-table').DataTable({
         processing: true,
         serverSide: true,
         ajax: "{{ route('alltoment') }}",
         columns: [
             { data: 'user_name', name: 'user_name' },
-            { data: 'flat_number', name: 'flat_number' },
-            { data: 'action', orderable: false, searchable: false }
+            { data: 'flat_number', name: 'flat_number' }
         ]
     });
 
-    // CREATE
+    // OPEN MODAL
     $('#createNewProduct').click(function () {
         $('#productForm')[0].reset();
-        $('#product_id').val('');
         $('#ajaxModel').modal('show');
     });
 
-    // EDIT
-    // $('body').on('click', '.editProduct', function () {
-    //     let id = $(this).data('id');
-
-    //     $.get("{{ route('allotment.edit', ':id') }}".replace(':id', id), function (data) {
-    //         $('#product_id').val(data.id);
-    //         $('select[name="user_id"]').val(data.user_id);
-    //         $('select[name="flat_id"]').val(data.flat_id);
-    //         $('#ajaxModel').modal('show');
-    //     });
-    // });
-
-    // SAVE
+    // SAVE (CREATE ONLY)
     $('#saveBtn').click(function (e) {
         e.preventDefault();
 
         $.post("{{ route('allotment.store') }}", $('#productForm').serialize(), function () {
             $('#ajaxModel').modal('hide');
             table.draw();
-        });
-    });
-
-    // DELETE
-    $('body').on('click', '.deleteProduct', function () {
-        if (!confirm('Are you sure?')) return;
-
-        let id = $(this).data('id');
-
-        $.ajax({
-            type: 'DELETE',
-            url: "{{ route('allotment.delete', ':id') }}".replace(':id', id),
-            success: function () {
-                table.draw();
-            }
         });
     });
 
