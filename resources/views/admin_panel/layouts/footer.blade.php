cf869281a882d32f3461b641d024114b473e4eb6
= <=>
2026-02-18 16:08:58 +0530

    change in footer

--- resources/views/admin_panel/layouts/footer.blade.php
+++ resources/views/admin_panel/layouts/footer.blade.php
@@ -3,13 +3,13 @@
 <br><br>
  <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><span>Socitey Management System</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
       <!-- All the links in the footer should remain intact. -->
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      Designed by <a href="https://www.saurabhinfosys.com/">Saurabh Infosys</a>
     </div>
   </footer><!-- End Footer -->
