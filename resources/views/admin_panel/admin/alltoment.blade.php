cee24532fc4a289ba4e50861ab5b29c1d30fff2a
= <=>
2026-02-18 15:00:46 +0530

    CC

--- resources/views/admin_panel/admin/alltoment.blade.php
+++ resources/views/admin_panel/admin/alltoment.blade.php
@@ -7,120 +7,144 @@
 </div>
 
 <div class="card">
-<div class="card-body mt-3">
+    <div class="card-body">
+        <br>
 
-<button class="btn btn-primary float-end" id="createNewProduct">Create New</button>
+        <a class="btn btn-primary float-end" href="javascript:void(0)" id="createNewProduct">
+            Create New
+        </a>
 
-<table class="table table-bordered data-table mt-4">
-<thead>
-<tr>
-    <th>User</th>
-    <th>House</th>
-</tr>
-</thead>
-<tbody></tbody>
-</table>
+        <h5 class="card-title">Bordered Table</h5>
 
-</div>
-</div>
+        <table class="table table-bordered border-primary data-table">
+            <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Flat No</th>
+                    <th width="280px">Action</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
 
-<!-- MODAL -->
-<div class="modal fade" id="ajaxModel">
-<div class="modal-dialog">
-<div class="modal-content">
+    <!-- MODAL -->
+    <div class="modal fade" id="ajaxModel" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
 
-<div class="modal-header">
-<h5 class="modal-title">Create Allotment</h5>
-<button class="btn-close" data-bs-dismiss="modal"></button>
-</div>
+                <div class="modal-header">
+                    <h4 class="modal-title">Create / Edit Allotment</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
 
-<div class="modal-body">
-<form id="productForm">
+                <div class="modal-body">
+                    <form id="productForm">
+                        <input type="hidden" name="product_id" id="product_id">
 
-<!-- BLOCK -->
-<div class="mb-3">
-<label>Block</label>
-<select class="form-select" id="block_id">
-<option value="">Select Block</option>
-@foreach($blocks as $block)
-<option value="{{ $block->id }}">{{ $block->block_no }}</option>
-@endforeach
-</select>
-</div>
+                        <div class="mb-3">
+                            <label>User</label>
+                            <select class="form-select" name="user_id">
+                                <option value="">Select User</option>
+                                @foreach($users as $user)
+                                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
-<!-- HOUSE -->
-<div class="mb-3">
-<label>House</label>
-<select class="form-select" name="flat_id" id="house_id">
-<option value="">Select House</option>
-</select>
-</div>
+                        <div class="mb-3">
+                            <label>Flat</label>
+                            <select class="form-select" name="flat_id">
+                                <option value="">Select Flat</option>
+                                @foreach($flats as $flat)
+                                    <option value="{{ $flat->id }}">{{ $flat->flat_number }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
-<!-- USER -->
-<div class="mb-3">
-<label>User</label>
-<select class="form-select" name="user_id">
-<option value="">Select User</option>
-@foreach($users as $user)
-<option value="{{ $user->id }}">{{ $user->name }}</option>
-@endforeach
-</select>
-</div>
+                        <button type="submit" class="btn btn-primary" id="saveBtn">
+                            Save
+                        </button>
 
-<button class="btn btn-primary" id="saveBtn">Save</button>
-</form>
-</div>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                            Close
+                        </button>
+                    </form>
+                </div>
 
-</div>
-</div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
+
+@section('scripts')
 <script>
 $(function () {
 
     $.ajaxSetup({
-        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
     });
 
+    // DATATABLE
     var table = $('.data-table').DataTable({
         processing: true,
         serverSide: true,
         ajax: "{{ route('alltoment') }}",
         columns: [
-            {data: 'user_name'},
-            {data: 'flat_number'}
+            { data: 'user_name', name: 'user_name' },
+            { data: 'flat_number', name: 'flat_number' },
+            { data: 'action', orderable: false, searchable: false }
         ]
     });
 
+    // CREATE
     $('#createNewProduct').click(function () {
         $('#productForm')[0].reset();
+        $('#product_id').val('');
         $('#ajaxModel').modal('show');
     });
 
-    // 🔹 Load houses by block
-    $('#block_id').change(function(){
-        let blockId = $(this).val();
-        $('#house_id').html('<option>Loading...</option>');
+    // EDIT
+    $('body').on('click', '.editProduct', function () {
+        let id = $(this).data('id');
 
-        $.get("{{ url('admin/get-houses') }}/" + blockId, function(data){
-            let options = '<option value="">Select House</option>';
-            data.forEach(function(house){
-                options += `<option value="${house.id}">${house.house_number}</option>`;
-            });
-            $('#house_id').html(options);
+        $.get("{{ route('allotment.edit', ':id') }}".replace(':id', id), function (data) {
+            $('#product_id').val(data.id);
+            $('select[name="user_id"]').val(data.user_id);
+            $('select[name="flat_id"]').val(data.flat_id);
+            $('#ajaxModel').modal('show');
         });
     });
 
-    // 🔹 Store allotment
-    $('#saveBtn').click(function(e){
+    // SAVE
+    $('#saveBtn').click(function (e) {
         e.preventDefault();
 
-        $.post("{{ route('allotment.store') }}", $('#productForm').serialize(), function(){
+        $.post("{{ route('allotment.store') }}", $('#productForm').serialize(), function () {
             $('#ajaxModel').modal('hide');
             table.draw();
         });
     });
 
+    // DELETE
+    $('body').on('click', '.deleteProduct', function () {
+        if (!confirm('Are you sure?')) return;
+
+        let id = $(this).data('id');
+
+        $.ajax({
+            type: 'DELETE',
+            url: "{{ route('allotment.delete', ':id') }}".replace(':id', id),
+            success: function () {
+                table.draw();
+            }
+        });
+    });
+
 });
 </script>
+@endsection
