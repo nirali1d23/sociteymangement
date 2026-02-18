3afb4ad04d5b8ad5c84aa235dace11eb0db1e0e9
= <=>
2026-02-18 17:08:38 +0530

    cc

--- app/Http/Controllers/AdminPanel/NoticeController.php
+++ app/Http/Controllers/AdminPanel/NoticeController.php
@@ -50,6 +50,8 @@ class NoticeController extends Controller
         $notice = new Notice();
         $notice->title = $request->input('title');
         $notice->description = $request->input('description');
+        $notice->start_date = $request->input('start_date');
+        $notice->time = $request->input('time');
         $notice->save();
 
         return redirect()->route('notice')->with('success', 'Notice created successfully.');
